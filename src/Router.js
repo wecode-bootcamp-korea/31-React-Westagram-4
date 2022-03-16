e33e49ff531e2f5d74595f323d1407689d3b4a43
bboyooning <bboyooning@gmail.com>
2022-03-16 19:36:21 +0900

    MODIFY: 컨벤션 수정

--- src/Router.js
+++ src/Router.js
@@ -1,41 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // 정수
-import LoginJung from './pages/jungsooKim/Login/Login' 
-import MainJung from './pages/jungsooKim/Main/Main' 
+import LoginJung from './pages/jungsooKim/Login/LoginJung';
+import MainJung from './pages/jungsooKim/Main/MainJung';
 // 보윤
-import LoginBo from './pages/boyoonKim/Login/Login' 
-import MainBo from './pages/boyoonKim/Main/Main' 
+import LoginBo from './pages/boyoonKim/Login/LoginBo';
+import MainBo from './pages/boyoonKim/Main/MainBo';
 // 희동
-import LoginHee from './pages/heedongChoi/Login/Login' 
-import MainHee from './pages/heedongChoi/Main/Main' 
+import LoginHee from './pages/heedongChoi/Login/LoginHee';
+import MainHee from './pages/heedongChoi/Main/MainHee';
 // 경현
-import LoginKyung from './pages/kyunghyunKim/Login/Login' 
-import MainKyung from './pages/kyunghyunKim/Main/Main' 
+import LoginKyung from './pages/kyunghyunKim/Login/LoginKyung';
+import MainKyung from './pages/kyunghyunKim/Main/MainKyung';
 // 정수
-import LoginSeo from './pages/seoyeongYoon/Login/Login' 
-import MainSeo from './pages/seoyeongYoon/Main/Main' 
+import LoginSeo from './pages/seoyeongYoon/Login/LoginSeo';
+import MainSeo from './pages/seoyeongYoon/Main/MainSeo';
 
-
-function Router () {
-  return ( 
+function Router() {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login-jung' element={<LoginJung />} />
-        <Route path='/main-jung' element={<MainJung />} />
-        <Route path='/login-bo' element={<LoginBo />} />
-        <Route path='/main-bo' element={<MainBo />} />
-        <Route path='/login-hee' element={<LoginHee />} />
-        <Route path='/main-hee' element={<MainHee />} />
-        <Route path='/login-kyung' element={<LoginKyung />} />
-        <Route path='/main-kyung' element={<MainKyung />} />
-        <Route path='/login-seo' element={<LoginSeo />} />
-        <Route path='/main-seo' element={<MainSeo />} />
+        <Route path="/jung/login" element={<LoginJung />} />
+        <Route path="/jung/main" element={<MainJung />} />
+        <Route path="/bo/login" element={<LoginBo />} />
+        <Route path="/bo/main" element={<MainBo />} />
+        <Route path="/hee/login" element={<LoginHee />} />
+        <Route path="/hee/main" element={<MainHee />} />
+        <Route path="/kyung/login" element={<LoginKyung />} />
+        <Route path="/kyung/main" element={<MainKyung />} />
+        <Route path="/seo/login" element={<LoginSeo />} />
+        <Route path="/seo/main" element={<MainSeo />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
+}
 
 export default Router;
--- src/pages/boyoonKim/Login/LoginBo.js
+++ src/pages/boyoonKim/Login/LoginBo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Login = () => {
-  return <div></div>;
+  return <div>하이</div>;
 };
 
 export default Login;
