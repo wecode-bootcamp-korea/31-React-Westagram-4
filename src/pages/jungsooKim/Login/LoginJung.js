eac608ea8221db4e28262cd86def0fb3bedbeea0
MAGHC <89845540+MAGHC@users.noreply.github.cog>
2022-03-17 15:05:07 +0900

    Add:개인작업페이지

--- src/pages/jungsooKim/Login/LoginJung.js
+++ src/pages/jungsooKim/Login/LoginJung.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import '../Login/LoginJung.scss';
+// import {Link} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+function Login() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/jung/main');
+  };
+
   return (
-    <div>
-      
+    <div class="Main">
+      <p class="changeFont">westagram</p>
+      <input
+        class="wrap id"
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+      />
+      <input class="wrap password" type="password" placeholder="비밀번호" />
+      <button class="wrap off button" type="button" onClick={goToMain}>
+        로그인
+      </button>
+      <p class="forget">비밀번호를 잊으셨나요?</p>
     </div>
   );
-};
+}
 
 export default Login;
--- src/pages/jungsooKim/Main/MainJung.js
+++ src/pages/jungsooKim/Main/MainJung.js
@@ -1,11 +1,230 @@
-import React from 'react';
+import '../Main/MainJung.scss';
 
-const Main = () => {
+function Main() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav className="Login">
+        <div className="navLeft">
+          <div className="Insata">
+            <img
+              alt="insta"
+              className="icon"
+              src="/image/jungsooKim/instagram.png"
+            />
+          </div>
+          <span className="changeFont2">Westagram</span>
+        </div>
+        <div className="SearchWrapper">
+          <input className="Search" type="text" placeholder="검색" />
+          <i className="fa-solid fa-magnifying-glass" />
+        </div>
+        <div className="navRight">
+          <img
+            alt="탐색"
+            className="icon"
+            src="/image/jungsooKim/explore.png"
+          />
+          <img
+            alt="좋아요"
+            className="icon"
+            src="/image/jungsooKim/heart.png"
+          />
+          <img
+            alt="프로필"
+            className="icon"
+            src="/image/jungsooKim/profile.png"
+          />
+        </div>
+      </nav>
+
+      <article className="mainBox">
+        <div className="top">
+          <img
+            alt="프로필"
+            src="/image/jungsooKim/칠.avif"
+            className="profileImage"
+          />
+          <span className="userName">UserName</span>
+          <img
+            alt="더보기"
+            className="moreIcon"
+            src="/image/jungsooKim/more.png"
+          />
+        </div>
+        <div className="mainFeed">
+          <div className="feedBOx">
+            <div className="middle">
+              <img
+                alt="피드"
+                className="middleImage"
+                src="/image/jungsooKim/andrew-romanov-qlgll3u2FTk-unsplash.jpg"
+              />
+            </div>
+            <div className="bottom">
+              <div className="bottom-Icon">
+                <img
+                  alt="아이콘하트"
+                  className="icon"
+                  src="/image/jungsooKim/heart after.png"
+                />
+                <img
+                  alt="아이콘말풍선"
+                  className="icon"
+                  src="/image/jungsooKim/balloon.png"
+                />
+                <img
+                  alt="아이콘보내기"
+                  className="icon"
+                  src="/image/jungsooKim/send.png"
+                />
+                <img
+                  alt="아이콘북마크"
+                  className="icon last"
+                  src="/image/jungsooKim/premium-icon-bookmarks-4218997.png"
+                />
+              </div>
+              <span>~님 외 10명이 좋아합니다.</span>
+              <span>더보기</span>
+              <p>30분전</p>
+            </div>
+          </div>
+          <div>
+            <p className="pushComment" />
+            <div className="comment" />
+            <input
+              className="commnetInput"
+              type="text"
+              placeholder="댓글달기..."
+            />
+            <button className="commentButton" type="button">
+              개시
+            </button>
+          </div>
+        </div>
+      </article>
+      <article className="mainRight">
+        <div className="top">
+          <img
+            alt="프로필"
+            className="profileImage"
+            src="/image/jungsooKim/다운로드.png"
+          />
+          <div className="profileSubscribe">
+            <p className="profileSub1">wecode_bootcamp</p>
+            <p className="profileSub2">wecode | 위코드</p>
+          </div>
+        </div>
+        <div className="storyMain">
+          <div className="story Top">
+            <span>스토리</span>
+            <span className="allView">모두보기</span>
+          </div>
+          <div className="storyContent">
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/일.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+              </div>
+            </div>
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/이.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+              </div>
+            </div>
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/삼.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="reCommandMain">
+          <div className="reCommand Top">
+            <span>회원님을 위한 추천</span>
+            <span className="allView">모두보기</span>
+          </div>
+          <div className="reCommandContent">
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/사.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+                <span className="follow">팔로우</span>
+              </div>
+            </div>
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/오.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+                <span className="follow">팔로우</span>
+              </div>
+            </div>
+            <div className="CommonBomx">
+              <img
+                alt="프로필"
+                className="profileImage"
+                src="/image/jungsooKim/육.avif"
+              />
+              <div className="discribe">
+                <span>
+                  id
+                  <br className="hour" />
+                  hour
+                </span>
+                <span className="follow">팔로우</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="information">
+          Instargram정보 지원 홍보센터 API 채용정보 개인정보처리 방침 약관
+          디렉터리 프로필 해시태그 언어 2019INSTARGRAM
+        </div>
+        <div className="divMainRight" />
+      </article>
+    </>
   );
-};
+}
 
 export default Main;
