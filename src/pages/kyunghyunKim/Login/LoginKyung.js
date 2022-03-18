0efa4ab8ca9dc0cc9702caffe7981d8e8dc985fe
김경현 <hyun@gimgyeonghyeon-ui-MacBookPro.local>
2022-03-18 20:14:30 +0900

    add:feature/kyunghyun branch 생성

--- src/pages/kyunghyunKim/Login/LoginKyung.js
+++ src/pages/kyunghyunKim/Login/LoginKyung.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-const Login = () => {
+import '../Login/LoginKyung.scss';
+
+function Login() {
   return (
-    <div>
-      
+    <div className="westagram">
+      <section class="westaContainer">
+        <div class="logo">westagram</div>
+        <div class="login">
+          <input
+            onInput="handleIdValue()"
+            id="userId"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            onInput="handleIdValue()"
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+        </div>
+        <button class="loginButton">로그인</button>
+      </section>
     </div>
   );
-};
+}
 
 export default Login;
--- src/pages/kyunghyunKim/Main/MainKyung.js
+++ src/pages/kyunghyunKim/Main/MainKyung.js
@@ -1,11 +1,162 @@
 import React from 'react';
+import '../Main/MainKyung.scss';
 
-const Main = () => {
+function Main() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav class="navbar">
+        <div class="navbar_logo">
+          <i class="fa-brands fa-instagram" />
+          <a href="!#">westagram</a>
+        </div>
+
+        <div class="navbarSearch">
+          <input type="search" placeholder="검색" />
+          <i class="fa-solid fa-magnifying-glass" />
+        </div>
+
+        <div class="navbar_icons">
+          <button>
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="탐색"
+            />
+          </button>
+          <button>
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="하트"
+            />
+          </button>
+          <button>
+            <img
+              src=" https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="마이페이지"
+            />
+          </button>
+        </div>
+      </nav>
+
+      {/* <main>
+        <div className="feeds">
+          <article>
+
+          </article>
+        </div>
+        <div className="main-right"></div>
+      </main> */}
+
+      <main>
+        <div class="feeds">
+          <article>
+            <div className="user">
+              <i class="fa-solid fa-circle-user" />
+              <span className="name">Kyunghyun</span>
+            </div>
+            <img
+              class="user_img"
+              src="https://images.unsplash.com/photo-1647372196772-b2839f41bd1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+              alt="피드이미지"
+            />
+
+            <div className="comment">
+              <div className="commentIcon">
+                <i class="fa-regular fa-heart" />
+                <i class="fa-regular fa-comment" />
+                <i class="fa-regular fa-share-from-square" />
+              </div>
+
+              <div className="commentLike">
+                <i class="fa-solid fa-circle-user" />
+                <p>
+                  <span className="name">k_hny</span>님 외 5명이 좋아합니다.
+                </p>
+              </div>
+
+              <ul className="commentLists">
+                <li>
+                  <span className="name">Kyunghyun</span>
+                  <span>위코드 안뇽</span>
+                </li>
+              </ul>
+
+              <form className="comments">
+                <input
+                  id="commentInput"
+                  type="text"
+                  placeholder="댓글 달기..."
+                />
+                <button id="submit">게시</button>
+              </form>
+            </div>
+          </article>
+        </div>
+
+        <aside>
+          <section className="mainRight">
+            <i class="fa-solid fa-circle-user" />
+            <div className="mainRightHeader">
+              <p className="name">wecode_bootcamp</p>
+              <p>WeCode | 위코드</p>
+            </div>
+          </section>
+
+          <div className="story">
+            <div className="storyHeader">
+              <p>스토리</p>
+              <p>모두보기</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">wecode1</p>
+              <p>5분전</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">wecode2</p>
+              <p>10분전</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">wecode3</p>
+              <p>15분전</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">wecode4</p>
+              <p>25분전</p>
+            </div>
+          </div>
+          <div className="recommand">
+            <div className="recommandHeader">
+              <p>회원님을 위한 추천</p>
+              <p>모두보기</p>
+            </div>
+            <div className="recommandInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">hi_wecode</p>
+              <p>팔로우</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">hi_wecode2</p>
+              <p>팔로우</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">hi_wecode3</p>
+              <p>팔로우</p>
+            </div>
+            <div className="storyInner">
+              <i class="fa-solid fa-circle-user" />
+              <p className="name">hi_wecode4</p>
+              <p>팔로우</p>
+            </div>
+          </div>
+        </aside>
+      </main>
+    </>
   );
-};
+}
 
 export default Main;
