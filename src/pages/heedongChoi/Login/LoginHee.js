f78dd683d4bad1c3e840b9e47991aea9b75dc05a
unknown <chkevi@gmail.com>
2022-03-19 16:13:36 +0900

    ADD: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/heedongChoi/Login/LoginHee.js
+++ src/pages/heedongChoi/Login/LoginHee.js
@@ -12,12 +12,25 @@ function LoginHee() {
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
 
+  const button = document.querySelector('button');
+
   function handleIdInput(event) {
     setId(event.target.value);
   }
 
   function handlePwInput(event) {
     setPw(event.target.value);
+  }
+
+  function handleButton() {
+    // if (id.includes('@') === true && pw.length > 5) {
+    //   button.style.backgroundColor = '#0095f6';
+    // } else {
+    //   button.style.backgroundColor = '#b2dffc';
+    // }
+    id.includes('@') === true && pw.length > 5
+      ? (button.style.backgroundColor = '#0095f6')
+      : (button.style.backgroundColor = '#b2dffc');
   }
 
   return (
@@ -32,12 +45,14 @@ function LoginHee() {
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               onChange={handleIdInput}
+              onInput={handleButton}
             />
             <input
               className="passwordInput"
               type="password"
               placeholder="비밀번호"
               onChange={handlePwInput}
+              onInput={handleButton}
             />
             <button className="loginButton" type="button" onClick={goToMain}>
               로그인
