78e9f2db5381107ff36e7fc16e5f104d1f85aceb
seo0yoon <genious921225@gmail.com>
2022-03-20 16:50:57 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/seoyeongYoon/Login/LoginSeo.js
+++ src/pages/seoyeongYoon/Login/LoginSeo.js
@@ -3,27 +3,31 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../../seoyeongYoon/Login/LoginSeo.scss';
 
 const Login = () => {
-  let [value, setValue] = useState('');
-
-  //State 하나 만든것,
-  //State를 만든이유? State에다가 데이터를 저장하라고(데이터바인딩용이)
+  let [idValue, setIdValue] = useState('');
+  let [pwValue, setPwValue] = useState('');
 
   const handleIdInput = evt => {
     console.log(evt.target.value);
-    // 콘솔에 찍어서 상태확인
-    // 여기서 오는 인자는 이벤트, 보통 e라고 표시
-    setValue(evt.target.value); //state 데이터를 수정할땐 두번째 인자에 넣음
+    setIdValue(evt.target.value);
   };
+  //State 하나 만든것,
+  //State를 만든이유? State에다가 데이터를 저장하라고(데이터바인딩용이)
+  // 콘솔에 찍어서 상태확인
+  // 여기서 오는 인자는 이벤트, 보통 e라고 표시
+  //state 데이터를 수정할땐 두번째 인자에 넣음
 
   const handlePwInput = evt => {
     console.log(evt.target.value);
-    setValue(evt.target.value);
+    setPwValue(evt.target.value);
   };
 
   const navigate = useNavigate();
   const goToLogin = () => {
     navigate('/seo/main');
   };
+
+  const isValid = idValue.includes('@') && pwValue.length >= 5; //버튼활성화조건
+
   return (
     <div className="login">
       <div className="container">
@@ -34,8 +38,8 @@ const Login = () => {
           <div className="input_box">
             <input
               id="id"
-              type="text"
-              onChange={handleIdInput}
+              type="email"
+              onChange={handleIdInput} //핸들어쩌고 함수 값이 변경될때, 발생
               placeholder="전화번호, 사용자 이름 또는 이메일"
             />
             <input
@@ -44,8 +48,9 @@ const Login = () => {
               placeholder="비밀번호를 입력하세요"
               onChange={handlePwInput}
             />
+            {/* 조건이 비활성화되야하니까 ! 부정 으로 만들어줌 */}
             <Link to="/seo/main">
-              <button type="submit" className="btn">
+              <button type="button" className="btn" disabled={!isValid}>
                 로그인
               </button>
             </Link>
